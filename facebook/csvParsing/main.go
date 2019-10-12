79341be72b14ebb9d37a5d828f8b11ad287025d3
Adriano <adriano.selaviles@gmail.com>
2019-10-11 17:06:25 -0700

    completed prod eng test

---
+++ facebook/csvParsing/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/csv"
+	"io"
+	"fmt"
+	"sort"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+type dinosaur struct {
+	name      string
+	legLen    string
+	diet      string
+	strideLen string
+	stance    string
+	speed     float64
+}
+
+func (d *dinosaur) computeSpeed() {
+	slen, _ := strconv.ParseFloat(d.strideLen, 64)
+	llen, _ := strconv.ParseFloat(d.legLen, 64)
+	d.speed = ((slen / llen) - 1) * math.Sqrt(llen*g)
+}
+
+const g = 9.81 // gravitational const
+
+func main() {
+	m := make(map[string]*dinosaur)
+
+	parseFile("dataset1.csv", func(record []string) {
+		if r, ok := m[record[0]]; ok {
+			r.legLen = record[1]
+			r.diet = record[2]
+		} else {
+			m[record[0]] = &dinosaur{
+				name:   record[0],
+				legLen: record[1],
+				diet:   record[2],
+			}
+		}
+	})
+
+	parseFile("dataset2.csv", func(record []string) {
+		if r, ok := m[record[0]]; ok {
+			r.strideLen = record[1]
+			r.stance = record[2]
+		} else {
+			m[record[0]] = &dinosaur{
+				name:      record[0],
+				strideLen: record[1],
+				stance:    record[2],
+			}
+		}
+	})
+
+	// get bipdedals and compute their speed
+	bipedals := []*dinosaur{}
+	for _, d := range m {
+		if d.stance == "bipedal" {
+			d.computeSpeed()
+			bipedals = append(bipedals, d)
+		}
+	}
+
+	// sort by fastest
+	sort.Slice(bipedals, func(i, j int) bool {
+		return bipedals[i].speed > bipedals[j].speed
+	})
+
+	// print out
+	for _, d := range bipedals {
+		fmt.Printf("%s\n", d.name)
+	}
+}
+
+// parse CSV file
+func parseFile(path string, handler func([]string)) {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("could not open csv file: %s", err)
+	}
+	r := csv.NewReader(f)
+	// chop off csv header row
+	if _, err := r.Read(); err != nil {
+		log.Fatal(err)
+	}
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		handler(record)
+	}
+}
