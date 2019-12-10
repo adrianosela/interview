297c29c4f9b4f297d4e8ecc2be3b4f9b997f4875
Adriano <adriano.selaviles@gmail.com>
2019-12-10 13:20:32 -0800

    add some runtime comments

---
+++ aws/finalRound/subtree_of_another_tree/encodeString.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/subtree-of-another-tree/ (easy)
+
+/*
+Time complexity : O(m^2+n^2+m*n) - A total of n nodes of the tree s and m nodes of tree t are traversed.
+		Assuming string concatenation takes O(k) time for strings of length k and indexOf takes O(m*n)
+Space complexity : O(max(m,n)) - The depth of the recursion tree can go upto n for tree t and m for tree s in worst case. 
+*/
+
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        String subTree = encodeTree(s);
+        String cmdTree = encodeTree(t);
+        return subTree.indexOf(cmdTree) > -1;
+    } 
+
+    // encodeTree encodes a tree into a nested string
+    // representation of its preorder traversal
+    public String encodeTree(TreeNode t) {
+        if (t == null) {
+                return "";
+        }
+        return "val(" + t.val + ")"
+            + "left(" + encodeTree(t.left) + ")"
+            + "right(" + encodeTree(t.right) + ")";
+    }
+}
--- aws/finalRound/subtree_of_another_tree/recursive.java
+++ aws/finalRound/subtree_of_another_tree/recursive.java
@@ -1,21 +1,22 @@
 // https://leetcode.com/problems/subtree-of-another-tree/ (easy)
 
-class Solution {
+/*
+Time complexity : O(m*n) - In worst case (skewed tree)
+Space complexity : O(n) - Recursion depth can go up to n where n = nodes in s
+*/
 
+class Solution {
     public boolean isSubtree(TreeNode s, TreeNode t) {
         if(s == null) {
             return false;
         }
-
         if (s.val != t.val) {
             return isSubtree(s.left, t) || isSubtree(s.right, t);
         }
-
         return check(s, t)
                 || isSubtree(s.left, t)
                 || isSubtree(s.right, t);
     }
-
     private boolean check(TreeNode s, TreeNode t) {
         if(s == null) {
             return t == null;
@@ -26,5 +27,4 @@ class Solution {
         }
         return s.val == t.val && check(s.left, t.left) && check(s.right, t.right);
     }
-
 }
